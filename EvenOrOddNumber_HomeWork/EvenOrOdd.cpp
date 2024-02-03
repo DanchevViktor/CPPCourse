5981c9a62bf624f9825e0a4d9295c4b01d318a8f
DanchevViktor <152903436+DanchevViktor@users.noreply.github.com>
2024-02-03 19:58:36 +0200

    Update

---
+++ EvenOrOddNumber_HomeWork/EvenOrOdd.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	cout << "Enter how many numbers you would like to check." << endl;
+	int number;
+	cin >> number;
+
+	for (int i = 0; i < number; i++) {
+
+		cout << "Enter number:" << endl;
+		
+		int n;
+		cin >> n;
+	
+		if (n % 2 == 0) {
+			cout << "Even" << endl;
+		}
+		else {
+			cout << "Odd" << endl;
+		}
+	}
+
+
+	return 0;
+}
+
--- EvenOrOddNumber_HomeWork/Task.h
+++ EvenOrOddNumber_HomeWork/Task.h
@@ -1,7 +1,4 @@
-﻿#include <iostream>
-using namespace std;
-
-/*
+﻿/*
 Задача 1: Напише следния код:
 
 Напишете програма на С++, която:
@@ -12,29 +9,3 @@ using namespace std;
 
 Ако числото е четно, на екрана се извежда “even”, ако е нечетно – “odd”
 */
-
-int main()
-{
-	cout << "Enter how many numbers you would like to check." << endl;
-	int number;
-	cin >> number;
-
-	for (int i = 0; i < number; i++) {
-
-		cout << "Enter number:" << endl;
-		
-		int n;
-		cin >> n;
-	
-		if (n % 2 == 0) {
-			cout << "Even" << endl;
-		}
-		else {
-			cout << "Odd" << endl;
-		}
-	}
-
-
-	return 0;
-}
-
