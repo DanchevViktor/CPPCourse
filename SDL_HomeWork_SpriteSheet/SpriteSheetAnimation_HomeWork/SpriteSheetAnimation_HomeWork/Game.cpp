7211246a8d0b2be0265c9879b4c07ae048254b6f
DanchevViktor <152903436+DanchevViktor@users.noreply.github.com>
2024-02-12 21:25:44 +0200

    SDL_Init

---
+++ SDL_HomeWork_SpriteSheet/SpriteSheetAnimation_HomeWork/SpriteSheetAnimation_HomeWork/Game.cpp
@@ -0,0 +1,82 @@
+#include "Game.h"
+
+Game::Game()
+{
+    Game::window = NULL;
+    Game::renderer = NULL;
+    Game::running = true;
+    Game::currentFrame = 0;
+}
+
+Game::~Game()
+{
+}
+
+bool Game::init(const char* title, int xpos, int ypos, int width, int height, int flags)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+        cout << "SDL Init success";
+        window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+        if (window != 0) {
+            cout << "Window created successfuly";
+            renderer = SDL_CreateRenderer(window, -1, 0);
+            if (renderer != 0) {
+                cout << "Renderer creation success";
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+
+            }
+            else {
+                cout << "Renderer init failed";
+                return false;
+            }
+        }
+        else {
+            cout << "Window init failed";
+            return false;
+        }
+    }
+    else {
+        cout << "SDL init failed";
+        return false;
+    }
+    cout << "Init success";
+    running = true;
+    return true;
+}
+
+void Game::render()
+{
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // white color
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+}
+
+void Game::update()
+{
+}
+
+void Game::handleEvents()
+{
+    SDL_Event event;
+    if (SDL_PollEvent(&event)) {
+        switch (event.type) {
+        case SDL_QUIT: running = false; break;
+        default: break;
+        }
+    }
+}
+
+void Game::clean()
+{
+    cout << "Cleaning game";
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+}
+
+bool Game::isRunning()
+{
+    return Game::running;
+}
+
